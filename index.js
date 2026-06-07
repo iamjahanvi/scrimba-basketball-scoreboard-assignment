dc20ad6f0ee8a9e113f7cbf9ec269babcd3bde87
Jahanvi Singh <135245356+iamjahanvi@users.noreply.github.com>
2026-06-07 15:45:12 +0530

    Adding project files exported from Scrimba

---
+++ index.js
@@ -0,0 +1,37 @@
+let countHome = 0
+let countGuest = 0
+
+let homeScore = document.getElementById("homeScore")
+let guestScore = document.getElementById("guestScore")
+
+function addHome1() {
+    countHome += 1
+    homeScore.textContent = countHome
+}
+
+function addHome2() {
+    countHome += 2
+    homeScore.textContent = countHome
+}
+
+function addHome3() {
+    countHome += 3
+    homeScore.textContent = countHome
+}
+
+
+function addGuest1() {
+    countGuest += 1
+    guestScore.textContent = countGuest
+}
+
+function addGuest2() {
+    countGuest += 2
+    guestScore.textContent = countGuest
+}
+
+function addGuest3() {
+    countGuest += 3
+    guestScore.textContent = countGuest
+}
+
